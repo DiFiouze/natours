d735e5fb54a1db1fed18ff63638fa5514c04e8e6
laurentravix <laurentravix@gmail.com>
2022-01-31 10:47:17 +0100

    feat: Router

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const fs = require('fs');
-const morgan = require('morgan')
+const morgan = require('morgan');
 
 const port = 3000;
 
@@ -12,137 +12,200 @@ app.use(morgan('dev'));
 app.use(express.json());
 
 app.use((req, res, next) => {
-    console.log('Hello from the middleware');
-    next();
-})
+  console.log('Hello from the middleware');
+  next();
+});
 
 app.use((req, res, next) => {
-    req.requestTime = new Date().toISOString();
-    next();
-})
+  req.requestTime = new Date().toISOString();
+  next();
+});
 
 // Route Handlers //
 
-// la méthode 'parse' converti un json en objet javascript  
-const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`));
+// la méthode 'parse' converti un json en objet javascript
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
 
 // Get All Tours //
 const getAllTours = (req, res) => {
-    console.log(req.requestTime);
-    res.status(200).json({
-        status: 'success',
-        requestAt: req.requestTime,
-        results: tours.length,
-        data: {
-            tours
-        }
-    });
-} 
+  console.log(req.requestTime);
+  res.status(200).json({
+    status: 'success',
+    requestedAt: req.requestTime,
+    results: tours.length,
+    data: {
+      tours,
+    },
+  });
+};
 
-// Get Tour // 
+// Get Tour //
 const getTour = (req, res) => {
-    console.log('get tour id', req.params);
+  console.log('get tour id', req.params);
 
-    // Converti l'id qui est à la base une string en nombre
-    // const id = req.params.id * 1;
-    const id = parseInt(req.params.id);
+  // Converti l'id qui est à la base une string en nombre
+  // const id = req.params.id * 1;
+  const id = parseInt(req.params.id);
 
-    if(id > tours.length) {
-        return res.status(404).json({
-            status: 'fail',
-            message: 'Invalid ID'
-        })
-    }
-
-    const tour = tours.find(el => el.id === id);
-
-    res.status(200).json({
-        status: 'success',
-        data: {
-            tour
-        }
+  if (id > tours.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
     });
-}
+  }
+
+  const tour = tours.find((el) => el.id === id);
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour,
+    },
+  });
+};
 
 // Create Tour //
 const createTour = (req, res) => {
-    //console.log(req.body);
+  //console.log(req.body);
 
-    // On crée un nouvel ID et on le stoque dans un nouvel objet
-    // qui contient la requête envoyée
-    const newId = tours[tours.length - 1].id + 1;
-    const newTour = Object.assign({id: newId} ,req.body);
+  // On crée un nouvel ID et on le stoque dans un nouvel objet
+  // qui contient la requête envoyée
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
 
-    // On pousse le nouvel objet dans le tableau
-    tours.push(newTour);
+  // On pousse le nouvel objet dans le tableau
+  tours.push(newTour);
 
-    // la méthode 'stringify' converti un objet javascript en json
-    fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), err => {
-        res.status(201).json({
-            status: "success",
-            data: {
-                tour: newTour
-            }
-        })
-    })
-}
+  // la méthode 'stringify' converti un objet javascript en json
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tour: newTour,
+        },
+      });
+    }
+  );
+};
 
 // Update Tour //
 const updateTour = (req, res) => {
-    if(parseInt(req.params.id) > tours.length) {
-        return res.status(404).json({
-            status: 'fail',
-            message: 'Invalid ID'
-        })
-    }
+  if (parseInt(req.params.id) > tours.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+  }
 
-    res.status(200).json({
-        status: "success",
-        data: {
-            tour: '<Updated tour here...>'
-        }
-    })
-}
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour: '<Updated tour here...>',
+    },
+  });
+};
 
 // Delete Tour //
 const deleteTour = (req, res) => {
-    if(parseInt(req.params.id) > tours.length) {
-        return res.status(404).json({
-            status: 'fail',
-            message: 'Invalid ID'
-        })
-    }
+  if (parseInt(req.params.id) > tours.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+  }
 
-    res.status(204).json({
-        status: "success",
-        data: null
-    })
-}
+  res.status(204).json({
+    status: 'success',
+    data: null,
+  });
+};
+
+
+// Users //
+
+const getAllUsers = (req, res) => {
+    res.status(500).json({
+        status: 'error',
+        message: 'This route is not yet defined'
+    });
+};
+
+const getUser = (req, res) => {
+    res.status(500).json({
+        status: 'error',
+        message: 'This route is not yet defined'
+    });
+};
+
+const createUser = (req, res) => {
+    res.status(500).json({
+        status: 'error',
+        message: 'This route is not yet defined'
+    });
+};
+
+const updateUser = (req, res) => {
+    res.status(500).json({
+        status: 'error',
+        message: 'This route is not yet defined'
+    });
+};
+
+const deleteUser = (req, res) => {
+    res.status(500).json({
+        status: 'error',
+        message: 'This route is not yet defined'
+    });
+};
+
+
 
 // Routing //
+
 //app.get('/api/v1/tours', getAllTours);
 //app.post('/api/v1/tours', createTour);
 //app.get('/api/v1/tours/:id', getTour);
 //app.patch('/api/v1/tours/:id', updateTour);
 //app.delete('/api/v1/tours/:id', deleteTour);
 
-
 // Routes //
+
 // Refactored Routing
-app
-.route('/api/v1/tours')
+const tourRouter = express.Router();
+const userRouter = express.Router();
+
+tourRouter
+.route('/')
 .get(getAllTours)
 .post(createTour);
 
-app
-.route('/api/v1/tours/:id')
-.get(getTour)
-.patch(updateTour)
-.delete(deleteTour);
+tourRouter
+  .route('/:id')
+  .get(getTour)
+  .patch(updateTour)
+  .delete(deleteTour);
+
+userRouter
+.route('/')
+.get(getAllUsers)
+.post(createUser);
+
+userRouter
+  .route('/:id')
+  .get(getUser)
+  .patch(updateUser)
+  .delete(deleteUser);
+
+app.use('/api/v1/tours', tourRouter)
+app.use('/api/v1/users', userRouter)
 
 // Start Server //
 // Listen //
 app.listen(port, () => {
-    console.log(`App running on port ${port}`);
-})
-
+  console.log(`App running on port ${port}`);
+});
